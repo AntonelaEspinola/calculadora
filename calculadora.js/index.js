0993a236593109e49837da0cf7c8394d84f1c3cc
AntonelaEspinola <antoespinola55@gmail.com>
2021-05-12 14:43:22 -0300

    Se creo el modulo calculadora

---
+++ calculadora.js/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    sumar = function(num1, num2){
+        return num1+ num2
+    },
+    restar = function(num1, num2){
+        return num1 -num2
+    },
+    multiplicar = function(num1, num2){
+        return num1 * num2
+    },
+    dividir = function(num1, num2){
+        return num1 / num2
+    }
+}
