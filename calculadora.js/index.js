c932454bdcea15e8bbca642e86215fa8444b5d90
AntonelaEspinola <antoespinola55@gmail.com>
2021-05-12 15:58:09 -0300

    se agrego la funcionalidad de seleccionar la accion a realizar desde la consola

--- calculadora.js/index.js
+++ calculadora.js/index.js
@@ -1,14 +1,13 @@
 module.exports = {
-    sumar = function(num1, num2){
-        return num1+ num2
-    },
-    restar = function(num1, num2){
-        return num1 -num2
-    },
-    multiplicar = function(num1, num2){
-        return num1 * num2
-    },
-    dividir = function(num1, num2){
-        return num1 / num2
-    }
+    sumar :(num1, num2)=>
+        num1+ num2,
+    
+    restar :(num1, num2)=>
+        num1 -num2,
+    multiplicar :(num1, num2)=>
+         num1 * num2,
+
+    dividir :(num1, num2)=>
+         num1 / num2,
+    
 }
